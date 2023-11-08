309b1618b9bfa043801c5f6fbf41644acbd8d802
momomms <122142516+momomms@users.noreply.github.com>
2023-11-08 18:28:40 -0500

    Update routeProduit.js
    Ajouter la pagination et des variables de query dans les routes de type GET qui le
    nécessitent

--- routes/routeProduit.js
+++ routes/routeProduit.js
@@ -1,11 +1,6 @@
 import { Router } from "express";
-
-const router=Router()
-
-//Importer les controllers
-
-import { creerProduit, lireProduit, mettreAJourProduit, supprimerProduit } from '../controllers/produitController.js';
-
+import Produit from '../models/produit.js'; // Importe le modele Produit
+const router = Router();
 
 // Créer un produit
 router.post('/produits', creerProduit);
@@ -19,4 +14,24 @@ router.put('/produits/:id', mettreAJourProduit);
 // Supprimer un produit par ID
 router.delete('/produits/:id', supprimerProduit);
 
+// Liste des produits avec pagination et des variables de requete
+router.get('/produits', (req, res) => {
+  const page = parseInt(req.query.page) || 1; 
+  const limit = parseInt(req.query.limit) || 10; 
+
+  // Utiliser "Produit" pour faire une requete de base de donnees avec pagination
+
+  Produit.findAndCountAll({
+    offset: (page - 1) * limit,
+    limit: limit,
+  })
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: 'Une erreur s\'est produite' });
+    });
+});
+
 export default router;
+
