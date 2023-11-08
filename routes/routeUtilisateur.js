4d5ea7c9fa140ce820856d96cb11ac839c47af56
momomms <122142516+momomms@users.noreply.github.com>
2023-11-08 18:27:16 -0500

    Update routeUtilisateur.js
    Ajouter la pagination et des variables de query dans les routes de type GET qui le
    nécessitent

--- routes/routeUtilisateur.js
+++ routes/routeUtilisateur.js
@@ -1,11 +1,8 @@
 import { Router } from "express";
-
-const router=Router()
-
-//Importer les controllers
-
 import { creerUtilisateur, lireUtilisateur, mettreAJourUtilisateur, supprimerUtilisateur } from '../controllers/utilisateurController.js';
+import Utilisateur from '../models/utilisateur.js'; // Importe le modele Utilisateur
 
+const router = Router();
 
 // Créer un utilisateur
 router.post('/utilisateurs', creerUtilisateur);
@@ -19,4 +16,25 @@ router.put('/utilisateurs/:id', mettreAJourUtilisateur);
 // Supprimer un utilisateur par ID
 router.delete('/utilisateurs/:id', supprimerUtilisateur);
 
+// Lire la liste des utilisateurs avec pagination et des variables de requete
+router.get('/utilisateurs', (req, res) => {
+  const page = parseInt(req.query.page) || 1; 
+  const limit = parseInt(req.query.limit) || 10; 
+
+  // Utiliser "Utilisateur" pour faire une requete de base de donnees avec pagination
+
+  Utilisateur.findAndCountAll({
+    offset: (page - 1) * limit,
+    limit: limit,
+  })
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: 'Une erreur sest produite' });
+    });
+});
+
 export default router;
+
+
